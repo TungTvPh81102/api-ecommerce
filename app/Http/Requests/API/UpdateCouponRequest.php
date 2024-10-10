7e04641784cba1d0c1c52505bcdb0526346a7700
TungTvPh81102 <quaixe121811@gmail.com>
2024-10-10 17:38:43 +0700

    commit feature/coupon-service

--- app/Http/Requests/API/UpdateCouponRequest.php
+++ app/Http/Requests/API/UpdateCouponRequest.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Requests\API;
 
+use App\Models\Coupon;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 class UpdateCouponRequest extends FormRequest
 {
@@ -11,7 +16,7 @@ class UpdateCouponRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,8 +26,37 @@ class UpdateCouponRequest extends FormRequest
      */
     public function rules(): array
     {
+        $coupon = $this->route('coupon');
+
+        if (is_string($coupon)) {
+            $coupon = Coupon::query()->find($coupon);
+        }
+
         return [
-            //
+            'name' => 'sometimes|required|string|min:3|max:255',
+            'code' => 'sometimes|required|string|min:3|max:255|unique:coupons,code,' . ($coupon->id ?? ''),
+            'type' => 'sometimes|required|in:fixed,percent',
+            'value' => 'sometimes|required|numeric|min:0',
+            'status' => 'sometimes|required|in:active,inactive',
+            'start_date' => 'sometimes|required|date|after_or_equal:today',
+            'expire_date' => 'sometimes|required|date|after:start_date',
+            'max_discount_percentage' => 'required_if:type,percent|numeric|min:0',
+            'min_order_amount' => 'sometimes|nullable|numeric|min:0',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors();
+
+        $response = response()->json([
+            'message' => $errors->messages()
+        ], Response::HTTP_BAD_REQUEST);
+
+        Log::error(__CLASS__ . '@' . __FUNCTION__, [
+            'message' => $errors->messages()
+        ]);
+
+        throw new HttpResponseException($response);
     }
 }
