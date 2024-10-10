8179ae7cbdbbe504dacf81b755623718a53bb7ed
TungTvPh81102 <quaixe121811@gmail.com>
2024-10-10 15:43:22 +0700

    done feature/product

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\StoreProductRequest;
+use App\Http\Requests\API\UpdateProductRequest;
 use App\Models\Product;
 use Cloudinary\Cloudinary;
 use Illuminate\Http\Request;
@@ -66,31 +67,27 @@ class ProductController extends Controller
             DB::beginTransaction();
             $dataProduct = $request->except('variants', 'image');
             $dataVariant = $request->variants;
-
             $dataProduct['slug'] = $dataProduct['name'] ? Str::slug($dataProduct['name'], '-') : null;
 
-            if ($request->hasFile('image')) {
-                $image = $request->file('image');
+            $image = $request->file('image') ?? null;
 
-                if ($image->isValid()) {
-                    $uploadResult = $this->cloudinary
-                        ->uploadApi()
-                        ->upload($image->getRealPath(), [
-                            'folder' => self::FOLDER_NAME
-                        ]);
-                    $dataProduct['image'] = $uploadResult['secure_url'];
-                } else {
-                    return response()->json([
-                        'message' => 'File không đúng định dạng'
-                    ], Response::HTTP_BAD_REQUEST);
-                }
+            if ($image->isValid()) {
+                $uploadResult = $this->cloudinary
+                    ->uploadApi()
+                    ->upload($image->getRealPath(), [
+                        'folder' => self::FOLDER_NAME
+                    ]);
+                $dataProduct['image'] = $uploadResult['secure_url'];
+            } else {
+                return response()->json([
+                    'message' => 'File không đúng định dạng'
+                ], Response::HTTP_BAD_REQUEST);
             }
 
             $product = Product::query()->create($dataProduct);
 
             if (!empty($dataVariant)) {
                 foreach ($dataVariant as $variant) {
-
                     $uploadedImages = [];
                     if (!empty($variant['image'])) {
                         foreach ($variant['image'] as $image) {
@@ -125,6 +122,15 @@ class ProductController extends Controller
             ], Response::HTTP_CREATED);
         } catch (\Exception $e) {
             DB::rollBack();
+
+            if (!empty($uploadResult['secure_url'])) {
+                $publicId = pathinfo(parse_url($uploadResult['secure_url'], PHP_URL_PATH), PATHINFO_FILENAME);
+                $publicIdWithFolder = self::FOLDER_NAME . '/' . $publicId;
+                $this->cloudinary
+                    ->uploadApi()
+                    ->destroy($publicIdWithFolder);
+            }
+
             Log::error(__CLASS__ . '@' . __FUNCTION__, [
                 'message' => $e->getMessage(),
                 'line' => $e->getLine(),
@@ -176,9 +182,82 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateProductRequest $request, string $id)
     {
-        //
+        try {
+            $product = Product::query()->find($id);
+            if (!$product) {
+                return response()->json([
+                    'message' => 'Không có dữ liệu'
+                ], Response::HTTP_NOT_FOUND);
+            }
+
+            $dataProduct = $request->except('variants', 'image');
+            $dataVariant = $request->variants;
+            $dataProduct['slug'] = $dataProduct['name'] ? Str::slug($dataProduct['name'], '-') : $product->slug;
+
+            $image = $request->file('image') ?? null;
+
+            if ($image->isValid()) {
+                $uploadResult = $this->cloudinary
+                    ->uploadApi()
+                    ->upload($image->getRealPath(), [
+                        'folder' => self::FOLDER_NAME
+                    ]);
+                $dataProduct['image'] = $uploadResult['secure_url'];
+            } else {
+                return response()->json([
+                    'message' => 'File không đúng định dạng'
+                ], Response::HTTP_BAD_REQUEST);
+            }
+
+            $product = Product::query()->ceate($dataProduct);
+
+            if (!empty($dataVariant)) {
+                foreach ($dataVariant as $variant) {
+                    $uploadedImages = [];
+                    if (!empty($variant['image'])) {
+                        foreach ($variant['image'] as $image) {
+                            if ($image->isValid()) {
+                                $uploadResult = $this->cloudinary
+                                    ->uploadApi()
+                                    ->upload($image->getRealPath(), [
+                                        'folder' => self::FOLODER_NAME_VARIANT
+                                    ]);
+                                $uploadedImages[] = $uploadResult['secure_url'];
+                            }
+                        }
+                    }
+
+                    $variant['image'] = json_encode($uploadedImages);
+
+                    $variantModel = $product->variants()->create($variant);
+
+                    if (!empty($variant['attribute_values'])) {
+                        foreach ($variant['attribute_values'] as $attributeValue) {
+                            $variantModel->attributeValues()->attach($attributeValue);
+                        }
+                    }
+                }
+            }
+
+            DB::commit();
+
+            return response()->json([
+                'message' => 'Thao tác thành công',
+                'data' => $product
+            ], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            Log::error(__CLASS__ . '@' . __FUNCTION__, [
+                'message' => $e->getMessage(),
+                'request' => $request->all(),
+                'line' => $e->getLine(),
+            ]);
+
+            return response()->json([
+                'message' => 'Lỗi server'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -209,4 +288,57 @@ class ProductController extends Controller
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
+
+    private function processProductData($request)
+    {
+        $dataProduct = $request->except('variants', 'image');
+        $dataProduct['slug'] = $dataProduct['name'] ? Str::slug($dataProduct['name'], '-') : null;
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            if ($image->isValid()) {
+                $uploadResult = $this->cloudinary
+                    ->uploadApi()
+                    ->upload($image->getRealPath(), [
+                        'folder' => self::FOLDER_NAME
+                    ]);
+                $dataProduct['image'] = $uploadResult['secure_url'];
+            } else {
+                return response()->json([
+                    'message' => 'File không đúng định dangjF'
+                ], Response::HTTP_BAD_REQUEST);
+            }
+        }
+        return $dataProduct;
+    }
+
+    private function processProductVariants($dataVariant, $product)
+    {
+        foreach ($dataVariant as $variant) {
+            $uploadedImages = [];
+            if (!empty($variant['image'])) {
+                foreach ($variant['image'] as $image) {
+                    if ($image->isValid()) {
+                        $uploadResult = $this->cloudinary
+                            ->uploadApi()
+                            ->upload($image->getRealPath(), [
+                                'folder' => self::FOLODER_NAME_VARIANT
+                            ]);
+                        $uploadedImages[] = $uploadResult['secure_url'];
+                    }
+                }
+            }
+
+            $variant['image'] = json_encode($uploadedImages);
+
+            $variantModel = $product->variants()->create($variant);
+
+            if (!empty($variant['attribute_values'])) {
+                foreach ($variant['attribute_values'] as $attributeValue) {
+                    $variantModel->attributeValues()->attach($attributeValue);
+                }
+            }
+        }
+    }
+
 }
